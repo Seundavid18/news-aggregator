d6b5fb655de3c109a9f337d1b661bc8fade2f947
David <oyewumidavid84@gmail.com>
2022-03-27 13:36:57 -0700

    deleted newsbody.jsx page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Home from './Home';
-import Newsbody from './Pages/Newsbody';
 import Tech from './Pages/Tech';
 import Politics from './Pages/Politics'
 import Entertainment from './Pages/Entertainment'
@@ -15,7 +14,6 @@ function App() {
     <Routes>
       <Route>
         <Route path='/' element={<Home />} />
-        <Route path=':newsbodyid' element={<Newsbody />} />
         <Route path="/tech" element={<Tech />} />
         <Route path="/politics" element={<Politics />} />
         <Route path="/entertainment" element={<Entertainment />} />
